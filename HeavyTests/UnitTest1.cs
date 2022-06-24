bb1348fa2b92108ca70d2be77d5d7a48e9df6db4
Tony Redondo <tony.redondo@datadoghq.com>
2022-06-24 13:30:01 +0200

    Updates

--- HeavyTests/UnitTest1.cs
+++ HeavyTests/UnitTest1.cs
@@ -28,26 +28,52 @@ public class UnitTest1
     }
     
     [Fact]
+    public void EmptyTest06()
+    {
+    }
+    
+    [Fact]
+    public void EmptyTest07()
+    {
+    }
+    
+    [Fact]
+    public void EmptyTest08()
+    {
+    }
+    
+    [Fact]
+    public void EmptyTest09()
+    {
+    }
+    
+    [Fact]
+    public void EmptyTest10()
+    {
+    }
+    
+    [Fact]
     public void HeavyTest()
     {
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Task.Run(() => Proc());
-        Proc();
+        var tsk1 = Task.Run(() => Proc());
+        var tsk2 = Task.Run(() => Proc());
+        var tsk3 = Task.Run(() => Proc());
+        var tsk4 = Task.Run(() => Proc());
+        var tsk5 = Task.Run(() => Proc());
+        var tsk6 = Task.Run(() => Proc());
+        var tsk7 = Task.Run(() => Proc());
+        var tsk8 = Task.Run(() => Proc());
+        var tsk9 = Task.Run(() => Proc());
+        var tsk10 = Task.Run(() => Proc());
+
+        Task.WaitAll(tsk1, tsk2, tsk3, tsk4, tsk5, tsk6, tsk7, tsk8, tsk9, tsk10);
     }
     
     private void Proc()
     {
-        // Test
         for (var x = 0; x < 60000; x++)
         {
-            for (var i = 0; i < 1_000_000; i++)
+            for (var i = 0; i < 120_000; i++)
             {
                 var a = 1000;
                 var b = 2000;
